0e0b60873d3353bafd5a146995d99e581794fbf1
Emil Bay <github@tixz.dk>
2018-10-05 10:32:57 +0200

    test ux

--- test.js
+++ test.js
@@ -2,15 +2,15 @@ var hs = require('.')
 var duplexify = require('duplexify')
 var through = require('through2')
 
-var a = spy('from server to client')
-var b = spy('from client to server')
+var a = spy('<-')
+var b = spy('->')
 
 var serverConnection = duplexify(a, b)
 var clientConnection = duplexify(b, a)
 
 function spy (label) {
   return through(function (chunk, enc, cb) {
-    console.log(label, chunk.toString())
+    console.log(label, chunk.some(b => b > 127) ? chunk.byteLength : chunk.toString())
     return cb(null, chunk)
   })
 }
